9f7f30d2c8203be0821aa7e3c3ae56fd8ff39cbb
Burhanuddin <personalburhanuddin7@gmail.com>
2025-10-05 20:57:07 +0500

    day-1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::prefix('seller')->group(function () {
+   Route::view('/OverView', 'seller.pages.gig1');
+});
